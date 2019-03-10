1cc025852a514cf14f523f8b813bd6fa93bacd85
Iurii Pelykh <yurp1980@gmail.com>
2019-03-10 17:37:14 +0200

    boost future for asio

--- examples/http_transport_examples.cpp
+++ examples/http_transport_examples.cpp
@@ -1,6 +1,9 @@
 
 #include <ccd/http/beast_transport.h>
 
+#include <boost/asio.hpp>
+#include <ccd/defs.h>
+
 #include <iostream>
 
 int main()
@@ -12,6 +15,30 @@ int main()
     r.host = "https://www.example.com";
     auto resp = t(r).get();
     //std::cout << resp.body;
+
+    using namespace std::literals;
+
+    boost::asio::io_context io_srv;
+    auto th = std::thread([&io_srv]
+    {
+        boost::asio::io_service::work w { io_srv };
+        io_srv.run();
+    });
+
+    boost::asio::steady_timer tm { io_srv };
+    tm.expires_after(5s);
+
+    std::cout << "1\n";
+    tm.async_wait(ccd::ftr).then([](boost::future<void> f)
+    {
+        f.get();
+        std::cout << "It WORKS!\n";
+    })
+    .get();
+    std::cout << "3\n";
+
+    io_srv.stop();
+    th.join();
 
     return 0;
 }
--- include/ccd/defs.h
+++ include/ccd/defs.h
@@ -17,11 +17,84 @@
 namespace ccd
 {
 
-template <class ...Ts>
+template<class ...Ts>
 using future_tuple = boost::future<std::tuple<boost::future<Ts>...>>;
 
 using key_value_list_t = std::vector<std::pair<std::string, std::string>>;
 using string_list_t = std::vector<std::string>;
 using timestamp_t = std::string;
+
+}
+
+// ---------------------------------------------------------------------------------------------------------------------
+
+namespace ccd
+{
+
+struct use_boost_future_t { };
+constexpr use_boost_future_t ftr;
+
+namespace details::asio_use_boost_future
+{
+
+template<typename> struct handler_selector;
+
+template<> struct handler_selector<void(boost::system::error_code)>
+{
+    using future_t = boost::future<void>;
+    boost::promise<void> m_promise;
+    void operator()(boost::system::error_code ec)
+    {
+        ec ? m_promise.set_exception(boost::system::system_error{ ec })
+           : m_promise.set_value();
+    }
+};
+
+template<typename T> struct handler_selector<void(boost::system::error_code, T)>
+{
+    using future_t = boost::future<T>;
+    boost::promise<T> m_promise;
+    void operator()(boost::system::error_code ec, T&& t)
+    {
+        ec ? m_promise.set_exception(boost::system::system_error{ ec })
+           : m_promise.set_value(std::forward<T>(t));
+    }
+};
+
+template<typename... Args> struct handler_selector<void(boost::system::error_code, Args...)>
+{
+    using future_t = boost::future<std::tuple<Args...>>;
+    boost::promise<std::tuple<Args...>> m_promise;
+    void operator()(boost::system::error_code ec, Args&& ... args)
+    {
+        ec ? m_promise.set_exception(boost::system::system_error{ ec })
+           : m_promise.set_value(std::forward_as_tuple(args...));
+    }
+};
+
+template<typename Signature>
+struct handler : handler_selector<Signature> { handler(const use_boost_future_t&) {} };
+
+}
+
+}
+
+#include <boost/asio/async_result.hpp>
+
+namespace boost::asio
+{
+
+template<typename Signature>
+class async_result<ccd::use_boost_future_t, Signature>
+{
+public:
+    using completion_handler_type = ccd::details::asio_use_boost_future::handler<Signature>;
+    using return_type = typename completion_handler_type::future_t;
+    explicit async_result(completion_handler_type& h) : m_future(h.m_promise.get_future()) { }
+    return_type get() { return std::move(m_future); }
+
+private:
+    return_type m_future;
+};
 
 }
